e5a9fa0f0876d248ce99d1e4ebd73df703ff55f4
CallMeJSON <matakk@Matakks-MacBook-Pro.local>
2023-06-06 23:00:36 +0200

    Added a few more components - added image from discord as tweet, correct height related to the text input with the use of scrollHeight. A new Button component

--- src/components/NewtweetForm.tsx
+++ src/components/NewtweetForm.tsx
@@ -1,19 +1,22 @@
 import { useSession } from "next-auth/react";
 import { Button } from "./Button";
 import { ProfileImage } from "./ProfileImage";
-import { useCallback, useLayoutEffect, useRef, useState } from "react";
+import { FormEvent, useCallback, useLayoutEffect, useRef, useState } from "react";
+import { api } from "~/utils/api";
 
 function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
     if (textArea == null) return;
     textArea.style.height = "0"
     textArea.style.height = `${textArea.scrollHeight}px`
 }
+
 export function NewTweetForm() {
     const session = useSession();
-    if (session.status != "authenticated") return;
+    if (session.status != "authenticated") return null;
 
     return <Form></Form>
 }
+
 function Form() {
     const session = useSession()
     const [inputValue, setInputValue] = useState('')
@@ -22,25 +25,40 @@ function Form() {
         updateTextAreaSize(textArea);
         textAreaRef.current = textArea;
     }, [])
+
     useLayoutEffect(() => {
         updateTextAreaSize(textAreaRef.current)
     }, [inputValue])
 
+    const createTweet = api.tweet.create.useMutation({
+        onSuccess:
+            newTweet => {
+                console.log(newTweet)
+                setInputValue('');
+            }
+    });
+
     if (session.status != "authenticated") return null;
 
-    return <form className="flex flex-col gap-2 border-b px-4 py-2">
-        <div className="flex gap-4">
-            <ProfileImage src={session.data.user.image} />
-            <textarea
-                ref={inputRef}
-                style={{ height: 0 }}
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-                className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
-                placeholder="What's happening?"
-            />
-        </div>
-        <Button className="self-end">Tweet</Button>
-    </form>
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault()
+        createTweet.mutate({ content: inputValue })
+    }
+    return (
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2 border-b px-4 py-2">
+            <div className="flex gap-4">
+                <ProfileImage src={session.data.user.image} />
+                <textarea
+                    ref={inputRef}
+                    style={{ height: 0 }}
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+                    placeholder="What's happening?"
+                />
+            </div>
+            <Button className="self-end">Tweet</Button>
+        </form>
+    )
 }
 
--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -10,7 +10,7 @@ export function ProfileImage({ src, className = "" }:
         <div className={`relative h-12 w-12 overflow-hidden rounded-full
             ${className}`}>
             {src == null ? null : (
-                <Image src={src} alt="Profile Image" quality={100} fill />
+                <Image src={src} alt="Profile Image" quality={100} fill/>
             )}
         </div>
     )
