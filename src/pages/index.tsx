2b1c8ec03b7a66291acecfccb37b6f9a9af39759
CallMeJSON <matakk@Matakks-MacBook-Pro.local>
2023-06-07 01:16:46 +0200

    Added functionallity for more comments/tweets from the tweet itself. But it doesn't work right now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 
 import { api } from "~/utils/api";
 import { NewTweetForm } from "~/components/NewTweetForm";
+import { InfiniteTweetList } from "~/components/InfiniteTweetList";
 
 const Home: NextPage = () => {
 
@@ -13,9 +14,29 @@ const Home: NextPage = () => {
       <header className="sticky top-0 z-10 border-b bg-white pt-2">
         <h2 className="mb-2 px-4 text-lg font-bold"></h2>
       </header>
-      <NewTweetForm/>
+      <NewTweetForm />
+      <RecentTweets />
     </>
   );
-}; 
+};
+
+function RecentTweets() {
+  const tweets = api.tweet.infiniteFeed.useInfiniteQuery(
+    {},
+    { getNextPageParam: (lastPage) => lastPage?.nextCursor }
+  );
+
+  return (
+    <InfiniteTweetList
+      tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
+      isError={tweets.isError}
+      isLoading={tweets.isLoading}
+      hasMore={tweets.hasNextPage}
+      fetchNewTweets={tweets.fetchNextPage}
+    />
+  );
+}
+
+
 
 export default Home;
